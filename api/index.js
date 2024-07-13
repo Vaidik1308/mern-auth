2217906e53eafb640678d4707076c630b4aa4a66
vaidik <vaidiksinghnirwan@gmail.com>
2024-07-13 12:50:00 +0530

    setup the folder structure such as router , controllers, and middleware folder in api folder for the backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const PORT = 3000
 const app = express()
+
+app.use("/api/user",userRoutes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
