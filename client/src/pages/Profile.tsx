529e341f5fcdf2c77e792be7cd81480a20435548
vaidik <vaidiksinghnirwan@gmail.com>
2024-08-07 11:20:43 +0530

    implemented the upload image functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,14 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
+import { app } from '../firebase';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+
+
 const Profile = () => {
   const {currentUser} = useSelector((state:RootState) => state.user);
   const [editEmail,setEditEmail] = useState(currentUser?.email)
   const [editUsername,setEditUsername] = useState(currentUser?.username)
+  const [imagePercent,setImagePercent] = useState(0)
+  const [imageError,setImageError] = useState(false)
+  const [formData,setFormData] = useState({})
   // const [editProfilePic,setEditProfilePic] = useState(currentUser?.profileImg)
+  const [image,setImage] = useState(null)
+  console.log(imagePercent);
+  
+  console.log(formData);
+  
+  const fileRef = useRef()  as React.MutableRefObject<HTMLInputElement>
+  console.log(image);
+  useEffect(() => {
+    if(image) {
+      handleFileUpload(image)
+    }
+  },[image])
+
+  const handleFileUpload = async (image) => {
+    // console.log(image);
+    const storage = getStorage(app);
+    const filename = new Date().getTime() + image.name;
+    const storageRef = ref(storage,filename);
+    const uploadTask = uploadBytesResumable(storageRef,image);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        setImagePercent(Math.round(progress))
+      },
+      (error) => {
+        setImageError(true)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({...FormData,profileImg:downloadURL})
+        })
+      }
+    )
+  }
   return (
     <main className='w-full flex items-center justify-center min-h-[80vh]'>
-      <section className='p-5 w-1/2 h-[60vh] shadow-lg'>
+      <section className='p-5 w-1/2 h-[65vh] shadow-lg'>
         <div className='border-b-[1px] pb-3'>
           <h2 className='text-3xl font-bold uppercase'>
             Your
@@ -18,7 +62,15 @@ const Profile = () => {
         <div className='my-8'>
           <form className='grid grid-cols-2 gap-x-8 gap-y-4 w-[85%] mx-auto' action="">
             <div className='col-span-2'>
-              <img className='size-32 object-cover rounded-full mx-auto' src={currentUser?.profileImg} alt="" />
+              <input onChange={(e) => setImage(e.target.files[0])} ref={fileRef} accept='image/*' type="file" hidden name="" id="" />
+              <img  onClick={() => fileRef.current.click()} className='size-32 object-cover rounded-full mx-auto' src={currentUser?.profileImg} alt="" />
+              <p className='w-full flex justify-center my-4 '>
+                {imageError ? (
+                  <span className='text-red-500 font-medium'>{"Error uploading image (file size < 2Mb), Please try again"}</span>
+                ) : (
+                  <span className='text-green-400 font-medium'>{imagePercent ? imagePercent+'%'+" uploaded" : null}</span>
+                )}
+              </p>
             </div>
             <div className='flex flex-col gap-1'>
               <label htmlFor="editMail">E-Mail:</label>
